61baf49689e9334281543f648dcab832c8ab086c
Ania <izumi454@gmail.com>
2018-12-03 22:04:39 +0100

    all-stuf

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -29,6 +29,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,7 +43,7 @@ namespace DatingApp.API
             {
                 app.UseHsts();
             }
-
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseHttpsRedirection();
             app.UseMvc();
         }
